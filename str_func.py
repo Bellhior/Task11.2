f9a92299d342b223520e6b6e9dbfe528d86554d7
Bellhior <cliff_burton85@mail.ru>
2023-05-30 13:15:53 +0400

    Add title function

--- str_func.py
+++ str_func.py
@@ -1,3 +1,9 @@
 def upper(words):
     upper_words = words.upper()
     return upper_words
+
+
+def title(words):
+    """Заглавные буквы каждого слова строки"""
+    titled_words = words.title()
+    return titled_words
